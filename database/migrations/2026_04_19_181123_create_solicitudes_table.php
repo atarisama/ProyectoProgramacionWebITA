56ad2afdaef35f4e5c6de5e0981e817875a9086d
MariaJose3535 <maria.jose.guerr23@gamil.com>
2026-04-19 12:29:18 -0600

    Migración detalle_solicitud

---
+++ database/migrations/2026_04_19_181123_create_solicitudes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('detalle_solicitud', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('solicitud_id')
+                  ->constrained('solicitudes')
+                  ->onDelete('cascade');
+
+            $table->foreignId('medicamento_id')
+                  ->nullable()
+                  ->constrained('medicamentos')
+                  ->onDelete('cascade');
+
+            $table->foreignId('material_id')
+                  ->nullable()
+                  ->constrained('materiales')
+                  ->onDelete('cascade');
+
+            $table->integer('cantidad');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_solicitud');
+    }
+};
